b1f293de502f679a93d4688255bbd8439fd469f4
Colin Copeland <copelco@caktusgroup.com>
2012-08-14 20:27:43 -0400

    add options for 0 guests; default to 2

--- rsvp/models.py
+++ rsvp/models.py
@@ -4,7 +4,15 @@ from datetime import date
 
 class Guest(models.Model):
 
-    GUEST_CHOICES = ((x, x) for x in xrange(1, 7))
+    GUEST_CHOICES = (
+        (0, 'Regrets'),
+        (1, 1),
+        (2, 2),
+        (3, 3),
+        (4, 4),
+        (5, 5),
+        (6, 6),
+    )
     DATE_CHOICES = (
         (date(2012, 10, 31), '10/31'),
         (date(2012, 11, 1), '11/1'),
@@ -12,7 +20,7 @@ class Guest(models.Model):
         (date(2012, 11, 3), '11/3'),
     )
     name = models.CharField(max_length=255)
-    party_size = models.PositiveIntegerField(choices=GUEST_CHOICES, default=1)
+    party_size = models.PositiveIntegerField(choices=GUEST_CHOICES, default=2)
     arrival_date = models.DateField(choices=DATE_CHOICES,
                                     default=date(2012, 11, 3))
     date_submitted = models.DateTimeField(auto_now_add=True)
